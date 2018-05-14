d9cd876e76c9ec3f054c2d4db4d5128455636a18
MouseDoc <sangyun3631@gmail.com>
2018-05-14 14:52:02 +0900

    Add base assets

---
+++ unity-ooyee/Assets/Scripts/Dialog/Core/DialogController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public abstract class DialogController : MonoBehaviour
+{
+    public DialogData Data { get; private set; }
+
+    public virtual void Build(DialogData data)
+    {
+        Data = data;
+    }
+}
---
+++ unity-ooyee/Assets/Scripts/Ingame/ScoreUIController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUIController : MonoBehaviour
+{
+    public Text Text
+    {
+        get { return GetComponent<Text>(); }
+    }
+
+    private int Score { set { Text.text = value.ToString(); } }
+
+    public void ChangeScore(int score)
+    {
+        Score = score;
+    }
+}
